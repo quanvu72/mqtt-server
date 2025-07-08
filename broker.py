c627bb08fb725f1d595013eae40d4d4e2846e1c8
quanvu72 <quan.vu2212829@hcmut.edu.vn>
2025-07-08 14:20:55 +0700

    server manager and save log

--- broker.py
+++ broker.py
@@ -1,9 +1,54 @@
-import asyncio
 import logging
-import os
+import asyncio
 import socket
-
+import os
+import json
+import paho.mqtt.client as paho
+from paho import mqtt
+from datetime import datetime
 from amqtt.broker import Broker
+from amqtt.client import MQTTClient
+from amqtt.mqtt.constants import QOS_1
+
+# Cấu hình logger để hiển thị thông tin
+formatter = "[%(asctime)s] :: %(levelname)s :: %(message)s"
+logging.basicConfig(level=logging.INFO, format=formatter)
+logger = logging.getLogger(__name__)
+latest_messages = {}
+LOG_FILE = datetime.now().strftime("%H%M%S_%d%m_log.json")
+
+#sửa lại phần không có sẵn file log
+def load_log():
+    if os.path.exists(LOG_FILE):
+        try:
+            with open(LOG_FILE, "r", encoding="utf-8") as f:
+                return json.load(f)
+        except Exception:
+            return {"clients": {}, "messages": []}
+    else:
+        return {"clients": {}, "messages": []}
+
+def save_log(log_data):
+    with open(LOG_FILE, "w", encoding="utf-8") as f:
+        json.dump(log_data, f, indent=2, ensure_ascii=False, default=str)
+
+def log_client_connection(client_id, client_address, conn_time):
+    log_data = load_log()
+    log_data["clients"][client_id] = {
+        "client_address": client_address,
+        "conn_time": conn_time
+    }
+    save_log(log_data)
+
+def log_message(topic, payload, timestamp):
+    log_data = load_log()
+    log_data["messages"].append({
+        #"client_id": client_id,
+        "topic": topic,
+        "payload": payload,
+        "timestamp": timestamp
+    })
+    save_log(log_data)
 
 def get_ipv4_address():
     try:
@@ -17,44 +62,78 @@ def get_ipv4_address():
         return f"Lỗi mạng: {e}"
     
 broker = get_ipv4_address()
-
-logger = logging.getLogger(__name__)
-
 config = {
-    "listeners": {
-        "default": {
-            "type": "tcp",
-            "bind": f"{broker}:1883",
-        }
+    'listeners': {
+        'default': {
+            'type': 'tcp',
+            'bind': f'{broker}:1883',
+        },
     },
-    "sys_interval": 10,
-    #"auth": {
-    #"allow-anonymous": True
-#}
-    
-    "auth": {
-        "allow-anonymous": True,
-        "password-file": os.path.join(
-            os.path.dirname(os.path.realpath(__file__)),
-            "passwd",
-        ),
-        "plugins": ["auth_file", "auth_anonymous"],
-    },
-    "topic-check": {"enabled": False},
+    'sys_interval': 10,
+    'auth': {
+        'allow-anonymous': True,
+        'password-file': None,
+        'plugins': ['auth_anonymous']
+    }
 }
 
+async def client_manager(broker):
+    known_clients = set()
+    while True:
+        await asyncio.sleep(3)
+        sessions = broker.sessions
+        if sessions:
+            logger.info("==================================================================")
+            logger.info(f"Client connected: {len(sessions)}")
+            for client_id, session_tuple in sessions.items():
+                session = session_tuple[0]
+                logger.info(f" - Client ID: {client_id}")
+                address = getattr(session, 'client_address', None)
+                conn_time = getattr(session, 'conn_time', None)
+                logger.info(f"   connect from: {address}")
+                logger.info(f"   connect time: {conn_time}")
+                # Ghi log client mới nếu chưa có
+                if client_id not in known_clients:
+                    log_client_connection(client_id, address, conn_time)
+                    known_clients.add(client_id)
+                msg = latest_messages.get(client_id)
+                if msg:
+                    logger.info(f"   Latest message: {msg}")
+            logger.info("==================================================================")
+        else:
+            logger.info("No clients connected.")
 
-async def test_coro() -> None:
+async def brokerGetMessage():
+    C = MQTTClient(client_id="get_message_client")
+    await C.connect(f'mqtt://{broker}:1883')
+    await C.subscribe([
+        ("#", QOS_1)
+    ])
+    #logger.info('Subscribed all!')
+    try:
+        while True:
+            message = await C.deliver_message()
+            packet = message.publish_packet
+            payload = packet.payload.data.decode('utf-8')
+            topic = packet.variable_header.topic_name
+            logger.info(f"Message and topic [{topic}]: {payload}")
+            log_message(topic, payload, datetime.now().isoformat())
+    except Exception as ce:
+        logger.error("Client exception : %s" % ce)
+
+async def main():
     broker = Broker(config)
     await broker.start()
-    # await asyncio.sleep(5)
-    # await broker.shutdown()
+    logger.info("Server start...")
+    
+    asyncio.create_task(client_manager(broker))
+    asyncio.create_task(brokerGetMessage())
 
+    while True:
+        await asyncio.sleep(1)
 
-if __name__ == "__main__":
-    formatter = "[%(asctime)s] :: %(levelname)s :: %(name)s :: %(message)s"
-    # formatter = "%(asctime)s :: %(levelname)s :: %(message)s"
-    logging.basicConfig(level=logging.INFO, format=formatter)
-    asyncio.get_event_loop().run_until_complete(test_coro())
-    asyncio.get_event_loop().run_forever()
-    # asyncio.run(test_coro())
+if __name__ == '__main__':
+        asyncio.run(main())
+        '''asyncio.get_event_loop().run_until_complete(main())
+        asyncio.get_event_loop().run_until_complete(brokerGetMessage())
+        asyncio.get_event_loop().run_forever()'''
